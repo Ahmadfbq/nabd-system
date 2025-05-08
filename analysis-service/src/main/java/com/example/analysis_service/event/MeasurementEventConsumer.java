34d079784537c68b6181ffa82cbdd562397a00a5
Ahmadfbq <ahmdb625@gmail.com>
2025-05-08 16:05:51 +0300

    fixed some issues

--- analysis-service/src/main/java/com/example/analysis_service/event/MeasurementEventConsumer.java
+++ analysis-service/src/main/java/com/example/analysis_service/event/MeasurementEventConsumer.java
@@ -1,6 +1,5 @@
 package com.example.analysis_service.event;
 import com.example.analysis_service.controller.AnalysisServiceImpl;
-<<<<<<< HEAD
 import com.example.health_data_service.view.MeasurementDto;
 import com.example.analysis_service.model.Analysis;
 import com.example.analysis_service.model.AnalysisMapper;
@@ -9,14 +8,11 @@ import com.example.analysis_service.config.RabbitMQConfig;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
-=======
 import org.springframework.stereotype.Service;
->>>>>>> origin/main
 
 @Service
 public class MeasurementEventConsumer {
     private final AnalysisServiceImpl analysis;
-<<<<<<< HEAD
     private final AnalysisMapper mapper;
 
     public MeasurementEventConsumer(AnalysisServiceImpl analysis, AnalysisMapper mapper) {
@@ -28,21 +24,20 @@ public class MeasurementEventConsumer {
     public void receiveMeasurement(MeasurementDto dto) {
         Analysis analysis = new Analysis();
         analysis.setTimestamp(LocalDateTime.now());
-        
+
         // Convert MeasurementDto to Measurement
         Measurement measurement = new Measurement();
         measurement.setType(dto.getType());
         measurement.setValue(dto.getValue());
         measurement.setTimestamp(dto.getTimestamp());
-        
+
         analysis.getMeasurements().add(measurement);
         this.analysis.checkForDanger(analysis);
         this.analysis.detectIllness(analysis);
-=======
-    @RabbitListener(queues = "${app.queue.easurement}");
-    public void receiveMeasurement(MeasurementDto dto) {
-        analysis.checkForDanger(dto);
-        analysis.detectIllness(dto);
->>>>>>> origin/main
+        @RabbitListener(queues = "${app.queue.easurement}") ;
+        public void receiveMeasurement (MeasurementDto dto){
+            analysis.checkForDanger(dto);
+            analysis.detectIllness(dto);
+        }
     }
 }
